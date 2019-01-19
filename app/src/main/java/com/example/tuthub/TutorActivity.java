fc918f7c380a9af1df3dca39d4251babc026b361
dixonwi3 <44004099+dixonwi3@users.noreply.github.com>
2019-01-19 02:00:52 -0500

    Merge branch 'master' of C:\Users\isagn\AndroidStudioProjects\TutHub with conflicts.

--- app/src/main/java/com/example/tuthub/TutorActivity.java
+++ app/src/main/java/com/example/tuthub/TutorActivity.java
@@ -1,5 +1,6 @@
 package com.example.tuthub;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -37,8 +38,26 @@ public class TutorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tutor);
 
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_dashboard:
+                        Intent goToDashboard = new Intent(TutorActivity.this, SearchScreenActivity.class);
+                        startActivity(goToDashboard);
+                        break;
+                    case R.id.navigation_notifications:
+                        Intent goToNotifications = new Intent(TutorActivity.this, SearchScreenActivity.class);
+                        startActivity(goToNotifications);
+                        break;
+
+
+                }
+                return false;
+            }
+        });
     }
 
 }
+
