00f7efee043279da878887475926ce2b538c7aac
Ryan E Felten <feltenry@msu.edu>
2019-01-19 17:18:09 -0500

    Set up SearchActivity outline

--- app/src/main/java/com/example/tuthub/Search.java
+++ app/src/main/java/com/example/tuthub/Search.java
@@ -5,6 +5,9 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Search extends AppCompatActivity {
@@ -23,7 +26,7 @@ public class Search extends AppCompatActivity {
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.navigation_profile:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
@@ -35,6 +38,18 @@ public class Search extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        Button searchBtn = (Button) findViewById(R.id.searchBtn);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText testing1EditText = (EditText) findViewById(R.id.testing1EditText);
+                EditText testing2EditText = (EditText) findViewById(R.id.testing2EditText);
+
+                String test1 = testing1EditText.getText().toString();
+                String test2 = testing2EditText.getText().toString();
+            }
+        });
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
