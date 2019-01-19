1c3254bfaae50a96e2b3161109038d9b26934dee
Evan <evanalexander012>
2019-01-19 00:45:17 -0500

    Renamed activity_main

--- app/src/main/java/com/example/tuthub/MainActivity.java
+++ app/src/main/java/com/example/tuthub/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.TutorActivity);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
