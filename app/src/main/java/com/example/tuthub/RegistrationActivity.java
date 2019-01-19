00890602f9ac233cdf7e1a15c7f98674f7a66b63
Ben Townsend <townsenb@mail.gvsu.edu>
2019-01-19 16:49:36 -0500

    popup in registration

--- app/src/main/java/com/example/tuthub/RegistrationActivity.java
+++ app/src/main/java/com/example/tuthub/RegistrationActivity.java
@@ -1,6 +1,7 @@
 package com.example.tuthub;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -112,13 +113,17 @@ public class RegistrationActivity extends AppCompatActivity {
         // inflate the layout of the popup window
         LayoutInflater inflater = (LayoutInflater)
                 getSystemService(LAYOUT_INFLATER_SERVICE);
-        View popupView = inflater.inflate(R.layout.popup_profile, null);
+        View popupView = inflater.inflate(R.layout.popup_register, null);
 
         // create the popup window
         int width = LinearLayout.LayoutParams.WRAP_CONTENT;
         int height = LinearLayout.LayoutParams.WRAP_CONTENT;
         boolean focusable = true; // lets taps outside the popup also dismiss it
         final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            popupWindow.setElevation(20);
+        }
 
         // show the popup window
         // which view you pass in doesn't matter, it is only used for the window tolken
