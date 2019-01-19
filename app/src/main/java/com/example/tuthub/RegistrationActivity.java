17a101cc682f1164f5ef11b27a9a4f75319bd416
dixonwi3 <44004099+dixonwi3@users.noreply.github.com>
2019-01-19 14:01:14 -0500

    Registration button

--- app/src/main/java/com/example/tuthub/RegistrationActivity.java
+++ app/src/main/java/com/example/tuthub/RegistrationActivity.java
@@ -1,10 +1,13 @@
 package com.example.tuthub;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
@@ -39,6 +42,15 @@ public class RegistrationActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Button registerBtn = (Button) findViewById(R.id.registerBtn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToProfileActivity = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(goToProfileActivity);
+            }
+        });
     }
 
 }
