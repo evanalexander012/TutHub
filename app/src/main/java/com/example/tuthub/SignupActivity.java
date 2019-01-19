c846bf4e93b7163a825e832d1340b51af17d837a
dixonwi3 <44004099+dixonwi3@users.noreply.github.com>
2019-01-19 03:10:56 -0500

    hello boys

--- app/src/main/java/com/example/tuthub/SignupActivity.java
+++ app/src/main/java/com/example/tuthub/SignupActivity.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
         Button openTutorActivityBtn = (Button) findViewById(R.id.loginBtn);
         openTutorActivityBtn.setOnClickListener(new View.OnClickListener() {
             @Override
